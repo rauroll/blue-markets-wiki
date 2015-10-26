b20cad95970a013de92fbe2dcec8bb6cd7d8c1bd
rauroll <olli.rauramo@aalto.fi>
2015-10-26 21:08:55 +0200

    clean comments

--- app/routes.js
+++ app/routes.js
@@ -1,27 +1,4 @@
 
-// var router = require('express').Router();
-
-// module.exports = function(app) {
-// 	app.get('/', function(req, res) {
-// 		console.log("Get request to /");
-// 		res.render('index');
-// 	})
-
-// 	.get('/templates/:name', function(req, res) {
-// 		var name = req.params.name;
-// 		console.log("Get request to /templates/" + name);
-// 		res.render('templates/' + name);
-// 	})
-
-// 	.get('*', function(req, res) {
-// 		console.log("Get request to /");
-// 		res.render('index');
-// 	});
-
-// }
-// module.exports = router;
-
-var jade = require('jade');
 
 module.exports = function(router) {
 
