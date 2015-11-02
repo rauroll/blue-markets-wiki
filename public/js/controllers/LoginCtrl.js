9e532940d447d2d296b5a15f76ad416ae8624b97
ollir <olli.rauramo@aalto.fi>
2015-11-02 12:43:38 +0200

    bugfixes, refactoring

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -1,75 +1,75 @@
 
 //angular.module('LoginCtrl', [])
-angular.module('bmwApp').controller('LoginController', ['$scope', 'AuthService', function($scope, AuthService) {
+angular.module('bmwApp').controller('LoginController', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService) {
 	
-    console.log(AuthService.getUserStatus());
+		console.log(AuthService.getUserStatus());
 
-    $scope.login = function () {
+		$scope.login = function () {
 
-      $scope.error = false;
-      $scope.disabled = true;
+			$scope.error = false;
+			$scope.disabled = true;
 
-      AuthService.login($scope.loginForm.username, $scope.loginForm.password)
+			AuthService.login($scope.loginForm.username, $scope.loginForm.password)
 
-        .then(function () {
-          $location.path('/account');
-          $scope.disabled = false;
-          $scope.loginForm = {};
-          //$scope.apply();
-        })
+				.then(function () {
+					$location.path('/account');
+					$scope.disabled = false;
+					$scope.loginForm = {};
+					//$scope.apply();
+				})
 
-        .catch(function () {
-          $scope.error = true;
-          $scope.errorMessage = "Invalid username or password";
-          $scope.disabled = false;
-          $scope.loginForm = {};
-        });
+				.catch(function () {
+					$scope.error = true;
+					$scope.errorMessage = "Invalid username or password";
+					$scope.disabled = false;
+					$scope.loginForm = {};
+				});
 
-    };
+		};
 }]).controller('LogoutController',
-  ['$scope', '$location', 'AuthService',
-  function ($scope, $location, AuthService) {
+	['$scope', '$location', 'AuthService',
+	function ($scope, $location, AuthService) {
 
-    $scope.user = AuthService.getUserStatus();
+		$scope.user = AuthService.getUserStatus();
 
-    $scope.logout = function () {
+		$scope.logout = function () {
 
-      console.log(AuthService.getUserStatus());
+			console.log(AuthService.getUserStatus());
 
 
-      AuthService.logout()
-        .then(function () {
-          $location.path('/');
-        });
+			AuthService.logout()
+				.then(function () {
+					$location.path('/');
+				});
 
-    };
+		};
 
 }]).controller('RegisterController',
-  ['$scope', '$location', 'AuthService',
-  function ($scope, $location, AuthService) {
+	['$scope', '$location', 'AuthService',
+	function ($scope, $location, AuthService) {
 
-    console.log(AuthService.getUserStatus());
+		console.log(AuthService.getUserStatus());
 
-    $scope.register = function () {
+		$scope.register = function () {
 
-      $scope.error = false;
-      $scope.disabled = true;
+			$scope.error = false;
+			$scope.disabled = true;
 
-      AuthService.register($scope.registerForm.username, $scope.registerForm.password)
+			AuthService.register($scope.registerForm.username, $scope.registerForm.password)
 
-        .then(function () {
-          $location.path('/login');
-          $scope.disabled = false;
-          $scope.registerForm = {};
-        })
+				.then(function () {
+					$location.path('/login');
+					$scope.disabled = false;
+					$scope.registerForm = {};
+				})
 
-        .catch(function () {
-          $scope.error = true;
-          $scope.errorMessage = "Something went wrong!";
-          $scope.disabled = false;
-          $scope.registerForm = {};
-        });
+				.catch(function () {
+					$scope.error = true;
+					$scope.errorMessage = "Something went wrong!";
+					$scope.disabled = false;
+					$scope.registerForm = {};
+				});
 
-    };
+		};
 
 }]);
--- public/js/services/services.js
+++ public/js/services/services.js
@@ -1,105 +1,93 @@
 angular.module('bmwApp').factory('AuthService',
-  ['$q', '$timeout', '$http',
-  function ($q, $timeout, $http) {
+	['$q', '$timeout', '$http',
+	function ($q, $timeout, $http) {
 
-    // create user variable
-    var user = null;
+		var user = null;
 
-    // return available functions for use in controllers
-    return ({
-      isLoggedIn: isLoggedIn,
-      getUserStatus: getUserStatus,
-      login: login,
-      logout: logout,
-      register: register
-    });
+		return ({
+			isLoggedIn: isLoggedIn,
+			getUserStatus: getUserStatus,
+			login: login,
+			logout: logout,
+			register: register
+		});
 
-    function isLoggedIn() {
-        if(user) {
-          return true;
-        } else {
-          return false;
-        }
-    }
+		function isLoggedIn() {
+				if(user) {
+					return true;
+				} else {
+					return false;
+				}
+		}
 
-    function getUserStatus() {
-      return user;
-    }
+		function getUserStatus() {
+			return user;
+		}
 
-    function login(username, password) {
+		function login(username, password) {
 
-      // create a new instance of deferred
-      var deferred = $q.defer();
+			var deferred = $q.defer();
 
-      // send a post request to the server
-      $http.post('/user/login', {username: username, password: password})
-        // handle success
-        .success(function (data, status) {
-          if(status === 200 && data.status){
-            user = true;
-            deferred.resolve();
-          } else {
-            user = false;
-            deferred.reject();
-          }
-        })
-        // handle error
-        .error(function (data) {
-          user = false;
-          deferred.reject();
-        });
+			$http.post('/user/login', {username: username, password: password})
+				.success(function (data, status) {
+					if(status === 200 && data.status){
+						user = true;
+						deferred.resolve();
+					} else {
+						user = false;
+						deferred.reject();
+					}
+				})
 
-      // return promise object
-      return deferred.promise;
+				.error(function (data) {
+					user = false;
+					deferred.reject();
+				});
 
-    }
+			return deferred.promise;
 
-    function logout() {
+		}
 
-      // create a new instance of deferred
-      var deferred = $q.defer();
+		function logout() {
 
-      // send a get request to the server
-      $http.get('/user/logout')
-        // handle success
-        .success(function (data) {
-          user = false;
-          deferred.resolve();
-        })
-        // handle error
-        .error(function (data) {
-          user = false;
-          deferred.reject();
-        });
+			var deferred = $q.defer();
 
-      // return promise object
-      return deferred.promise;
+			$http.get('/user/logout')
 
-    }
+				.success(function (data) {
+					user = false;
+					deferred.resolve();
+				})
 
-    function register(username, password) {
+				.error(function (data) {
+					user = false;
+					deferred.reject();
+				});
 
-      // create a new instance of deferred
-      var deferred = $q.defer();
+			return deferred.promise;
 
-      // send a post request to the server
-      $http.post('/user/register', {username: username, password: password})
-        // handle success
-        .success(function (data, status) {
-          if(status === 200 && data.status){
-            deferred.resolve();
-          } else {
-            deferred.reject();
-          }
-        })
-        // handle error
-        .error(function (data) {
-          deferred.reject();
-        });
+		}
 
-      // return promise object
-      return deferred.promise;
+		function register(username, password) {
 
-    }
+			var deferred = $q.defer();
+
+			$http.post('/user/register', {username: username, password: password})
+
+				.success(function (data, status) {
+					if(status === 200 && data.status){
+						deferred.resolve();
+					} else {
+						deferred.reject();
+					}
+				})
+
+				.error(function (data) {
+					deferred.reject();
+				});
+
+			return deferred.promise;
+
+		}
 
 }]);
